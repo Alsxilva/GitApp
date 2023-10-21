4de66cafd9db093a5a50e46508838481883f79d4
Alejandro Silva <alsxilva@gmail.com>
2023-10-21 14:11:07 -0600

    finished class

--- XcodeApps/RPS/RPS/GameViewController.swift
+++ XcodeApps/RPS/RPS/GameViewController.swift
@@ -11,19 +11,75 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var computerChoice: UILabel!
+    @IBOutlet weak var resultPrint: UILabel!
+    
+    enum Choice: String{
+        case rock = "Rock"
+        case paper = "Paper"
+        case scissor = "Scissor"
     }
-    */
-
+    
+    enum Winner: String{
+        case user = "You won"
+        case computer = "CPU won"
+        case tie = "Tie"
+    }
+    
+    let choicess:[Choice] = [.rock, .paper, .scissor]
+    
+    @IBAction func userChpice(_ sender: UIButton) {
+        let userChoice = sender.tag
+        switch userChoice{
+        case 1:
+            resultPrint.text = whosTheWinner(user: .rock, computer: computerChoiced()).rawValue
+            break
+        case 2:
+            resultPrint.text = whosTheWinner(user: .paper, computer: computerChoiced()).rawValue
+            break
+        case 3:
+            resultPrint.text = whosTheWinner(user: .scissor, computer: computerChoiced()).rawValue
+            break
+        default:
+            print("Pick one right")
+        }
+    }
+    
+    func computerChoiced() -> Choice{
+        return choicess.randomElement()!
+    }
+    
+    func whosTheWinner(user: Choice, computer: Choice) -> Winner{
+        
+        computerChoice.text = computer.rawValue
+        
+        switch user{
+        case .rock:
+            if computer == .scissor {
+                return .user
+            } else if computer == .paper{
+                return .computer
+            } else {
+                return .tie
+            }
+        case .paper:
+            if computer == .rock {
+                return .user
+            } else if computer == .scissor{
+                return .computer
+            } else {
+                return .tie
+            }
+        case .scissor:
+            if computer == .paper {
+                return .user
+            } else if computer == .rock{
+                return .computer
+            } else {
+                return .tie
+            }
+        }
+    }
 }
--- XcodeApps/RPS/RPS/ViewController.swift
+++ XcodeApps/RPS/RPS/ViewController.swift
@@ -12,8 +12,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        buttonPlay.isEnabled = false
     }
+    
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var buttonPlay: UIButton!
+    @IBOutlet weak var scoreButtonPressed: UIButton!
+    @IBOutlet weak var matchesButtonPressed: UIButton!
+    
+    //Game mode settings
 
-
+    @IBOutlet weak var winValueField: UITextField!
+    @IBAction func loseValueField(_ sender: UITextField) {
+    }
+    @IBOutlet weak var toWinValueField: UITextField!
+    
+    
+    //Hide and show game info fields (unfinished)
+    
+    @IBOutlet weak var stackSliderWidth: NSLayoutConstraint!
+    @IBOutlet weak var stackInputsWidth: NSLayoutConstraint!
+    
+    
+    @IBAction func userFilledName(_ sender: UITextField) {
+        buttonPlay.isEnabled = true
+        if (sender.text == ""){ buttonPlay.isEnabled = false
+        }
+    }
+    
+    @IBAction func scoreButtonPressed(_ sender: UIButton) {
+        self.stackSliderWidth.constant = 50
+        self.stackInputsWidth.constant = 50
+    }
+    
+    @IBAction func matchesButtonPressed(_ sender: UIButton) {
+        self.stackSliderWidth.constant = 50
+        self.stackInputsWidth.constant = 50
+    }
 }
-
